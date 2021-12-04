1458a570413ca507e1170aa9f53a7c3f6b9c12df
sudousernameavailable <andreadegruttola99@gmail.com>
2021-12-04 19:34:06 +0100

    implementazione classe UnacceptableKeyException

---
+++ ProjectSE/src/projectse/UnacceptableKeyException.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectse;
+
+/**
+ *
+ * @author Group3
+ */
+public class UnacceptableKeyException extends RuntimeException {
+
+    /**
+     * Creates a new instance of <code>UnacceptableException</code> without
+     * detail message.
+     */
+    public UnacceptableKeyException() {
+    }
+
+    /**
+     * Constructs an instance of <code>UnacceptableException</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public UnacceptableKeyException(String msg) {
+        super(msg);
+    }
+}
